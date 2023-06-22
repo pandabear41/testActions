ec61ba1f8e50565119616d31537d3b59d43fc20f
Panda <pandabear41@users.noreply.github.com>
2023-06-22 15:39:05 -0700

    Fix error

--- Envs.js
+++ Envs.js
@@ -6,6 +6,6 @@ var admin_user_idsid = process.env.USER_ID;
 var admin_user_password = process.env.USER_PASS;
 
 exports.MainEnv = { 
-    Host: env_host, 
+    Host: host, 
     Auth: admin_user_idsid + ':' + admin_user_password
 }
